583af75b0405f066c131db3b7032da5ad31a1d63
Eric <ericleixd@gmail.com>
2018-08-06 15:14:05 +0800

    Initial Commit

---
+++ SJTU_Robot_control_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SJTU_Robot_control_app
+//
+//  Created by Eric Lei on 6/8/2018.
+//  Copyright © 2018 Eric Lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
